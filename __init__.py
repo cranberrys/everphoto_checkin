da86cac7a25dce90d4ddc0f7a6502a8ea4bacc44
Skogen <muumlover@live.com>
2020-12-02 09:11:38 +0800

    module new way

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
-from .everphoto_checkin import everphoto_checkin as get_app
+from .everphoto_checkin import EverphotoCheckin
 
 plug_info = {
     'title': '时光相册',
+    'class': EverphotoCheckin
 }
--- everphoto_checkin.py
+++ everphoto_checkin.py
@@ -1,7 +1,5 @@
-import os
-
 from ac_api import AcApplication
-from module.everphoto_checkin.handle.everphoto_api import EverPhoto
+from .handle.everphoto_api import EverPhoto
 from .handle.handle_task import ECView, ec_task_action, ec_task_add, ec_task_smscode, ConfigView
 from .handle.scheduler import auto_check_in
 
@@ -16,20 +14,21 @@ async def init_callback(app):
         app.cron_job.add(key='auto_check_in', func=auto_check_in, **cron_job)
 
 
-def everphoto_checkin():
-    static_path = os.path.dirname(os.path.abspath(__file__)) + '/template'
-    app = AcApplication()
-    app.ac_set_static_path(static_path)
+class EverphotoCheckin(AcApplication):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    app.router.add_view('/task', ECView, name='index')
-    app.router.add_view('/config', ConfigView, name='config')
-    app.router.add_get('/add', ec_task_add, name='ec_task_add')
-    app.router.add_post('/action', ec_task_action, name='ec_task_action')
-    app.router.add_post('/smscode', ec_task_smscode, name='ec_task_smscode')
-    app.router.add_view('/task/{id}', ECView, name='ec_task_item')
+        self.ac_set_template_path('/template')
+        self.ac_set_static_path('/static')
 
-    app.ac_set_init_callback(init_callback)
-    return app
+        self.router.add_view('/task', ECView, name='index')
+        self.router.add_view('/config', ConfigView, name='config')
+        self.router.add_get('/add', ec_task_add, name='ec_task_add')
+        self.router.add_post('/action', ec_task_action, name='ec_task_action')
+        self.router.add_post('/smscode', ec_task_smscode, name='ec_task_smscode')
+        self.router.add_view('/task/{id}', ECView, name='ec_task_item')
+
+        self.ac_set_init_callback(init_callback)
 
 
 if __name__ == '__main__':
